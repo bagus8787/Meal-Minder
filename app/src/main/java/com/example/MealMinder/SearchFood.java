c0c1f2d4f3cc61912557bf024cc58da16f7d986f
fia <rofiatulqosimah@gmail.com>
2024-06-06 15:56:29 +0700

    food data

--- app/src/main/java/com/example/MealMinder/SearchFood.java
+++ app/src/main/java/com/example/MealMinder/SearchFood.java
@@ -36,6 +36,33 @@ public class SearchFood extends AppCompatActivity {
         foodDataList.add(new FoodData("Mie Goreng", "380 kkal", R.drawable.mie_goreng));
         foodDataList.add(new FoodData("Mie Ayam", "421 kkal", R.drawable.mie_ayam));
         foodDataList.add(new FoodData("Bakso", "218 kkal", R.drawable.bakso));
+        foodDataList.add(new FoodData("Tahu Tek", "387 kkal", R.drawable.tahutek));
+        foodDataList.add(new FoodData("Kerak Telor", "400 kkal", R.drawable.keraktelor));
+        foodDataList.add(new FoodData("Soto Betawi", "135 kkal", R.drawable.sotobetawi));
+        foodDataList.add(new FoodData("Pizza 1 Slice", "192 kkal", R.drawable.pizza));
+        foodDataList.add(new FoodData("Lasagna", "510 kkal", R.drawable.lasagna));
+        foodDataList.add(new FoodData("Gado Gado", "318 kkal", R.drawable.gado_gado));
+        foodDataList.add(new FoodData("Rawon", "288 kkal", R.drawable.rawon));
+        foodDataList.add(new FoodData("Sate Ayam", "225 kkal", R.drawable.sate_ayam));
+        foodDataList.add(new FoodData("Sate Kambing", "216 kkal", R.drawable.sate_kambing));
+        foodDataList.add(new FoodData("Pempek", "234 kkal", R.drawable.pempek));
+        foodDataList.add(new FoodData("Rendang", "195 kkal", R.drawable.rendang));
+        foodDataList.add(new FoodData("Sop Buntut", "68 kkal", R.drawable.sop_buntut));
+        foodDataList.add(new FoodData("Ikan Bakar", "260 kkal", R.drawable.ikan_bakar));
+        foodDataList.add(new FoodData("Capcay", "120 kkal", R.drawable.capcay));
+        foodDataList.add(new FoodData("Bubur Ayam", "372 kkal", R.drawable.bubur_ayam));
+        foodDataList.add(new FoodData("Pecel Lele", "292 kkal", R.drawable.pecel_lele));
+        foodDataList.add(new FoodData("Siomay", "103 kkal", R.drawable.siomay));
+        foodDataList.add(new FoodData("Mie Aceh", "238 kkal", R.drawable.mie_aceh));
+        foodDataList.add(new FoodData("Bebek Goreng", "674 kkal", R.drawable.bebek));
+        foodDataList.add(new FoodData("Soto Ayam", "312 kkal", R.drawable.soto_ayam));
+        foodDataList.add(new FoodData("Air Mineral", "0 kkal", R.drawable.airmineral));
+        foodDataList.add(new FoodData("Jus Stroberi", "38 kkal", R.drawable.jusstroberi));
+        foodDataList.add(new FoodData("Pop Ice", "100 kkal", R.drawable.popice));
+        foodDataList.add(new FoodData("Es Teh", "90 kkal", R.drawable.esteh));
+        foodDataList.add(new FoodData("Matcha Latte", "190 kkal", R.drawable.matchalatte));
+        foodDataList.add(new FoodData("Kopi Gula Aren", "150 kkal", R.drawable.kopisusugulaaren));
+
 
         setFoodRecycler(foodDataList);
 
